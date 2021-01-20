ff6ddc596c3e861f2dadc8ddcafa263c64d663e7
mdsrt1 <mdsrt1@users.noreply.github.com>
2021-01-20 15:05:30 +0600

    portfolio

--- my/js/main.js
+++ my/js/main.js
@@ -40,7 +40,7 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{})
-sr.reveal('.home__button',{delay: 200})
+sr.reveal('.home__button',{})
 sr.reveal('.home__img',{delay: 400})
 sr.reveal('.home__social-icon',{interval: 200})
 
